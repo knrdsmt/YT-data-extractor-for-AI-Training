7758963331151514f7c48c5040584e5f40ff9b61
knrdsmt <97449172+knrdsmt@users.noreply.github.com>
2024-03-22 13:19:22 +0100

    Commit 1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydub import AudioSegment
 import time
 
 
-def normalize_volume(folder_path, audio_file, target_volume=-30):
+def adjust_volume(folder_path, audio_file, target_volume=-30):
     audio_path = os.path.join(folder_path, audio_file)
     audio = AudioSegment.from_mp3(audio_path)
     audio_volume = audio.dBFS
@@ -16,12 +16,12 @@ def normalize_volume(folder_path, audio_file, target_volume=-30):
     audio.export(output_path, format="mp3")
 
 
-def change_sampling_rate(audio_path, sampling_rate=16000):
+def change_sampling(audio_path, sampling_rate=16000):
     audio = AudioSegment.from_file(audio_path)
     audio = audio.set_frame_rate(sampling_rate)
     audio.export(audio_path, format="mp3")
-    changed_audio = AudioSegment.from_file(audio_path)
-    print("New sampling rate:", changed_audio.frame_rate, "Hz")
+    modified_audio = AudioSegment.from_file(audio_path)
+    print("New sampling rate:", modified_audio.frame_rate, "Hz")
 
 
 def clean_filename(filename):
@@ -62,15 +62,15 @@ def split_audio(audio_path, transcript, speaker_id, output_folder, film_id):
             if end_time < clip1.duration_seconds - 0.1:
                 end_time += 0.1
             split_clip = clip.subclip(start_time, end_time)
-            output_folder_path = os.path.join(output_folder, f'{speaker_id}_{film_id}_{i+1}.mp3')
+            save_folder = os.path.join(output_folder, f'{speaker_id}_{film_id}_{i+1}.mp3')
 
-            # Check if there are profanities in the transcript
+            # Check if the transcript contains any profanity
             if not exclude_line(entry['text']):
-                split_clip.write_audiofile(output_folder_path)
+                split_clip.write_audiofile(save_folder)
             else:
-                print(f"Excluded line in transcript for sample {i+1}.")
+                print(f"Detected exclusion condition in the transcript for sample {i+1}.")
         else:
-            print(f"Sample time out of range for audio file for sample {i+1}.")
+            print(f"Sample time out of the audio file range for sample {i+1}.")
 
     clip.close()
 
@@ -106,7 +106,7 @@ def convert_transcript(transcript, transcript_path):
             if not exclude_line(text):
                 file.write(f"{i+1}: {text}\n")
             else:
-                print(f"Removed line in transcript for sample {i+1}.")
+                print(f"Removed line in the transcript for sample {i+1}.")
 
 
 def remove_file(file_path):
@@ -126,22 +126,22 @@ def create_speaker_legend(folder, legend_filename, legend):
     legend_path = os.path.join(folder, legend_filename)
     with open(legend_path, 'w', encoding='utf-8') as file:
         for entry in legend:
-            file.write(f"{entry['speaker_id']} {entry['name']}\n")
+            file.write(f"{entry['idM']} {entry['name']}\n")
 
 
-def create_film_legend(folder, legend_filename, legend):
+def create_movie_legend(folder, legend_filename, legend):
     os.makedirs(folder, exist_ok=True)
     legend_path = os.path.join(folder, legend_filename)
     with open(legend_path, 'w', encoding='utf-8') as file:
         for entry in legend:
-            file.write(f"{entry['speaker_id']} {entry['film_id']} {entry['name']}\n")
+            file.write(f"{entry['idM']} {entry['idF']} {entry['name']}\n")
 
 
 def main():
-    youtube_url_list = ["list of youtube film"]
+    youtube_url_list = ["https://www.youtube.com/watch?v=4VugmKYsCLs", "https://www.youtube.com/watch?v=svekxPKoABk", "https://www.youtube.com/watch?v=DqXZ-014JGg&t=5s"]
     speakers_legend = []
-    films_legend = []
-    lang = 'en'
+    movies_legend = []
+    lang = 'pl'
 
     for youtube_url in youtube_url_list:
         speaker_id = None
@@ -149,27 +149,50 @@ def main():
         output_folder = None
 
         yt = YouTube(youtube_url)
-
+        
         _id = youtube_url.split("=")[1].split("&")[0]
         try:
             transcript = YouTubeTranscriptApi.get_transcript(_id, languages=[lang])
         except NoTranscriptFound:
-            print("Transcript in English is not available for this video.")
+            print("Transcription in Polish is not available for this video.")
+
 
         if transcript:
-            # Check if speaker already exists in the legend
+            # Check if the speaker already exists in the legend
             channel_name = clean_filename(yt.author)
             existing_speaker_entry = next((entry for entry in speakers_legend if entry['name'] == channel_name), None)
 
             if existing_speaker_entry:
-                speaker_id = existing_speaker_entry['speaker_id']
+                speaker_id = existing_speaker_entry['idM']
             else:
-                speaker_id = f"S{len(speakers_legend) + 1:02d}"
-                speakers_legend.append({'speaker_id': speaker_id, 'name': channel_name})
+                speaker_id = f"M{len(speakers_legend) + 1:02d}"
+                speakers_legend.append({'idM': speaker_id, 'name': channel_name})
 
-            # Check if film already exists in the legend
-            film_name = clean_filename(yt.title)
-            film_id = f"F{len(films_legend) + 1:02d}"
-            films_legend.append({'film_id': film_id, 'speaker_id': speaker_id, 'name': film_name})
+            # Check if the movie already exists in the legend
+            movie_name = clean_filename(yt.title)
+            film_id = f"F{len(movies_legend) + 1:02d}"
+            movies_legend.append({'idF': film_id, 'idM':speaker_id, 'name': movie_name})
 
-        audio_path, speaker_folder,
+
+
+        audio_path, speaker_folder, yt, audio_name = download_audio(f'{speaker_id}', youtube_url)
+        # Adjust volume (this may not change much as it adjusts only the main file, not the splits)
+        change_sampling(audio_path, sampling_rate=16000)
+        transcript = download_transcript(youtube_url, speaker_folder, speaker_id, film_id)
+
+
+        if transcript:
+            output_folder = f'{speaker_id}'
+            os.makedirs(output_folder, exist_ok=True)
+
+            split_audio(audio_path, transcript, speaker_id, output_folder, film_id)
+
+        # Remove the downloaded audio file
+        # remove_file(audio_path)
+
+    create_speaker_legend("Legends", "speakers_legend.txt", speakers_legend)
+    create_movie_legend("Legends", "movies_legend.txt", movies_legend)
+
+if __name__ == "__main__":
+    main()
+
